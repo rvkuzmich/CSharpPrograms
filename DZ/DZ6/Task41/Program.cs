26501486880aed1cf995f226db18c11b6104b80b
Roman Kuzmich <rvku2mich@yandex.ru>
2023-03-01 21:44:56 +0300

    Added DZ6

---
+++ DZ/DZ6/Task41/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int[] CreateArray(string text)
+{
+    System.Console.WriteLine(text);
+    int[] array = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+    return array;
+}
+void PrintArray(int[] array)
+{
+    System.Console.WriteLine("[" + string.Join(", ", array) + "]");
+}
+void CountPositive(int[] array)
+{
+    int count = 0;
+    for (int i=0; i<array.Length; i++)
+    {
+        if (array[i]>0) count++;
+    }
+    System.Console.WriteLine($"Количество введенных чисел больше нуля - {count}");;
+}
+
+int[] newArray = CreateArray("Введите желаемые числа через пробел");
+PrintArray(newArray);
+CountPositive(newArray);
---
+++ DZ/DZ6/Task43/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double[] DefineLine(string text)
+{
+    System.Console.WriteLine(text);
+    double[] array = Array.ConvertAll(Console.ReadLine()!.Split(), double.Parse);
+    return array;
+}
+void CrossCoords(double[] array)
+{
+    double x = Math.Round((array[2]-array[0])/(array[1]-array[3]), 1);
+    double y = Math.Round(array[1]*x+array[0], 1);
+    System.Console.WriteLine($"Точка пересечения двух прямых находится в координатах {x}; {y}");
+}
+double[] newArray = DefineLine("Введите значения коэффициентов прямых b1, k1, b2 и k2 через пробел");
+CrossCoords(newArray);
